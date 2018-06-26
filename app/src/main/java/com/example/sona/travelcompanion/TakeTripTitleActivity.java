17ac3efbb2cf133c5ef10a139b9f721539d5f6f9
sonalikasahoo <sonalikasahoo6@gmail.com>
2018-06-26 17:05:05 +0530

    calender added to select date

--- app/src/main/java/com/example/sona/travelcompanion/TakeTripTitleActivity.java
+++ app/src/main/java/com/example/sona/travelcompanion/TakeTripTitleActivity.java
@@ -5,6 +5,8 @@ import java.util.Calendar;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
+import android.app.Dialog;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
@@ -13,18 +15,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class TakeTripTitleActivity extends AppCompatActivity {
 
     //UI References
+    private DatePicker datePicker;
+    private int year, month, day;
+
     private EditText etFromDate;
     private EditText etToDate;
     private EditText etTripTitle;
-
-    private DatePickerDialog fromDatePickerDialog;
-    private DatePickerDialog toDatePickerDialog;
-
-    private SimpleDateFormat dateFormatter;
 
     Button btnCreateTrip;
 
@@ -33,11 +34,21 @@ public class TakeTripTitleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_trip_title);
 
-
-        dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.UK);
         findViewsById();
 
-        //setDateTimeField();
+        etFromDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setDate(v);
+            }
+        });
+
+        etToDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setDate(v);
+            }
+        });
 
         btnCreateTrip.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,48 +59,59 @@ public class TakeTripTitleActivity extends AppCompatActivity {
                 Log.d("pikachu", "onClick: "+tripTitle+" "+fromDate+" "+toDate);
             }
         });
+
+
     }
 
     private void findViewsById() {
         etTripTitle = findViewById(R.id.etTripTitle);
         etFromDate = findViewById(R.id.etFromdate);
-        etFromDate.setInputType(InputType.TYPE_NULL);
-        etFromDate.requestFocus();
-
         btnCreateTrip = findViewById(R.id.btnCreateTrip);
 
         etToDate = findViewById(R.id.etToDate);
-        etToDate.setInputType(InputType.TYPE_NULL);
     }
 
-    private void setDateTimeField() {
-        etFromDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-            }
-        });
-        etToDate.setOnClickListener((View.OnClickListener) this);
-
-        Calendar newCalendar = Calendar.getInstance();
-        fromDatePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
-
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                Calendar newDate = Calendar.getInstance();
-                newDate.set(year, month, dayOfMonth);
-                etFromDate.setText(dateFormatter.format(newDate.getTime()));
-            }
-        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
-
-        toDatePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
-
-            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                Calendar newDate = Calendar.getInstance();
-                newDate.set(year, monthOfYear, dayOfMonth);
-                etToDate.setText(dateFormatter.format(newDate.getTime()));
-            }
-
-        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+    @SuppressWarnings("deprecation")
+    public void setDate(View view) {
+        showDialog(999);
+        Toast.makeText(getApplicationContext(), "ca",
+                Toast.LENGTH_SHORT)
+                .show();
     }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        // TODO Auto-generated method stub
+        if (id == 999) {
+            DatePickerDialog dialog  =new DatePickerDialog(this,
+                    myDateListener, year, month, day);
+            dialog.getDatePicker().setMinDate(System.currentTimeMillis());
+            return dialog;
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new
+            DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker arg0,
+                                      int arg1, int arg2, int arg3) {
+                    // TODO Auto-generated method stub
+                    // arg1 = year
+                    // arg2 = month
+                    // arg3 = day
+                    showDate(arg1, arg2+1, arg3);
+                }
+            };
+
+    private void showDate(int year, int month, int day) {
+        if(etFromDate.hasFocus())
+            etFromDate.setText(new StringBuilder().append(day).append("/")
+                .append(month).append("/").append(year));
+        else
+            etToDate.setText(new StringBuilder().append(day).append("/")
+                    .append(month).append("/").append(year));
+    }
+
 }
